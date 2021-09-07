a0c88e29bc5036c911bd21bf3cb143e0839a9fcd
Helder Avila <helderavila2016@gmail.com>
2021-09-07 18:02:53 -0300

    Função para transferir tokens

--- Token.sol
+++ Token.sol
@@ -9,6 +9,8 @@ contract Token {
     
     mapping(address => uint256) public balanceOf;
     
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
     constructor(string memory _name, string memory _symbol, uint _decimals, uint _totalSypply) {
         name = _name;
         symbol = _symbol;
@@ -16,5 +18,19 @@ contract Token {
         totalSupply = _totalSypply;
         balanceOf[msg.sender] = totalSupply;
     }
+    
+    function transfer(address _to, uint256 _value) external returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0));
+        balanceOf[_from] = balanceOf[_from] - (_value);
+        balanceOf[_to] = balanceOf[_to] + (_value);
+        emit Transfer(_from, _to, _value);
+    }
+    
     
 }
